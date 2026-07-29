d86455c29ba18f5238ddd3473885655fda395518
Steven Enamakel <enamakel@tinyhumans.ai>
2026-07-29 11:28:36 +0000

    test(daemon): split admission lifecycle coverage

--- src/sdk/src/daemon/tests/mod.rs
+++ src/sdk/src/daemon/tests/mod.rs
@@ -22,6 +22,7 @@ use super::mappers::HarnessSemanticEvent;
 use super::providers::{RunTaskFn, RunTaskOptions, RunTaskResult};
 use super::*;
 
+mod admission_tests;
 mod capability_tests;
 mod provider_tests;
 mod system_info_tests;
